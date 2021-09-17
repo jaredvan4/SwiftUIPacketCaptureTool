5878b85de832a549601d084b38e7fe5e8f86d5ad
Jared VanEnkevort <55300416+jaredvan4@users.noreply.github.com>
2021-09-17 15:01:24 -0400

    Initial Commit

---
+++ SeniorProj/SeniorProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeniorProjApp.swift
+//  SeniorProj
+//
+//  Created by Jared on 9/17/21.
+//
+
+import SwiftUI
+
+@main
+struct SeniorProjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
