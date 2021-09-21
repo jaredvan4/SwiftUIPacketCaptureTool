0fadb7c8f863963f852d0224e409762717393a50
Jared VanEnkevort <55300416+jaredvan4@users.noreply.github.com>
2021-09-21 15:08:57 -0400

    removed unused files

--- SeniorProj/Views/MainView.swift
+++ SeniorProj/Views/MainView.swift
@@ -8,9 +8,9 @@
 import SwiftUI
 
 struct MainView: View {
-    let titleStr = String(PcapMainClassWrapper().getNum())
     var testDevices :[NetworkInterface] = [NetworkInterface(iPv4Address: "127.0.0.1", deviceName: "some fake device", status: false),NetworkInterface(iPv4Address: "1.1.1.1.1.1.1.0", deviceName: "another fake test device", status: true)]
     var body: some View {
+        Text(String(PcapMainClassWrapper().getNum()))
         VSplitView {
             NetworkInterfaceSelectorView(devices:testDevices)
         }.frame(minWidth: 600, minHeight: 400).frame(width: nil)
